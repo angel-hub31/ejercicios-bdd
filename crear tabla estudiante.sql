fc678bad0fdcd478ae51e2f9fd76cb31a8043488
angel-hub31 <moralesmales1@hotmail.com>
2026-05-12 10:16:31 -0500

    se crea la llave primaria id_esstudiantes

--- crear tabla estudiante.sql
+++ crear tabla estudiante.sql
@@ -1,7 +1,7 @@
 
 
 --eliminar tabal
-
+drop table esstudiantes;
 --crear tabla
 create table esstudiantes(
 id_estudiante int,
@@ -9,5 +9,7 @@ nombre varchar (50),
 apellido varchar (50),
 edad int,
 curso varchar (50),
-fecha_registro varchar(10)
-);
+fecha_registro varchar(10),
+
+constraint esstudiantes_pk primary key(id_estudiante)
+)

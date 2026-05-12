445826f3709c4f1ae0f0de5020f57f18e8e2eae9
angel-hub31 <moralesmales1@hotmail.com>
2026-05-12 10:09:00 -0500

    se crea la tabla estudiantes

---
+++ crear tabla estudiante.sql
@@ -0,0 +1,13 @@
+
+
+--eliminar tabal
+
+--crear tabla
+create table esstudiantes(
+id_estudiante int,
+nombre varchar (50),
+apellido varchar (50),
+edad int,
+curso varchar (50),
+fecha_registro varchar(10)
+);
